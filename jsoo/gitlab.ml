9710c6b03cc3ed509a79de379199c9b40be474f6
Tim McGilchrist <timmcgil@gmail.com>
2021-09-17 15:13:22 +1000

    Initial JSOO support.

---
+++ jsoo/gitlab.ml
@@ -0,0 +1,13 @@
+module Time = struct
+  let now = Unix.gettimeofday
+
+  let sleep = Js_of_ocaml_lwt.Lwt_js.sleep
+end
+
+module Env = struct
+  let debug = false
+
+  let gitlab_uri = "https://gitlab.com/api/v4"
+end
+
+include Gitlab_core.Make (Env) (Time) (Cohttp_lwt_jsoo.Client)
