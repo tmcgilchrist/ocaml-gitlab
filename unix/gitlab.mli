324bd9006f35436c3f15ec292c9ffdf5067c0792
Antonin Décimo <antonin@tarides.com>
2022-03-14 15:45:50 +0100

    Document GITLAB_ env vars via Cmdliner

--- unix/gitlab.mli
+++ unix/gitlab.mli
@@ -1,1 +1,7 @@
 include Gitlab_s.Gitlab
+
+module Env : sig
+  val envs : Cmdliner.Cmd.Env.info list
+  (** [envs] is a list of environment variables that influence the
+      library's behaviour. *)
+end
