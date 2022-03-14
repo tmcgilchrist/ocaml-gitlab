3f085212f116d6d732ebfdfd9fefaa62ac760673
Antonin Décimo <antonin@tarides.com>
2022-03-14 15:45:50 +0100

    Document GITLAB_ env vars via Cmdliner

--- unix/gitlab.ml
+++ unix/gitlab.ml
@@ -9,6 +9,14 @@ module Env = struct
 
   let gitlab_uri =
     try Unix.getenv "GITLAB_URL" with _ -> "https://gitlab.com/api/v4"
+
+  let envs =
+    let open Cmdliner in
+    let doc = "The GitLab instance to connect to." in
+    let gitlab_url = Cmd.Env.info "GITLAB_URL" ~doc in
+    let doc = "Enable debugging (anything that's not 0)." in
+    let gitlab_debug = Cmd.Env.info "GITLAB_DEBUG" ~doc in
+    [gitlab_debug; gitlab_url]
 end
 
 include Gitlab_core.Make (Env) (Time) (Cohttp_lwt_unix.Client)
