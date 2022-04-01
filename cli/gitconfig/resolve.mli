327e557a4096d73c3afdcc3c410c58a97329bb05
Olivier Nicole <olivier@chnik.fr>
2022-04-01 17:02:36 +0200

    Add functions for extracting Gitlab project name.

---
+++ cli/gitconfig/resolve.mli
@@ -0,0 +1,14 @@
+(** Search for a [.git] directory from the current directory upwards. Return
+    [None] if none could be found. *)
+val find_dot_git : unit -> string option
+
+(** Search for a [.git] directory from the current directory upwards, and parse
+    it.  Return [Error] with an error message if none could be found, or in
+    case of parse error. *)
+val parse_dot_git : unit -> (Types.config, string) result
+
+(** [gitlab_project_name ~remote config] extracts the Gitlab project name from
+    the URL of the remote named [remote] in [config]. If [remote] is not
+    specified, and there is more than one remote, returns [None]. Also returns
+    [None] if no suitable remote URL can be found.  *)
+val gitlab_project_name : ?remote:string -> Types.config -> string option
