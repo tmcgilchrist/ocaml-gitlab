314ee017707681d91a93590303b5b430766dadd3
Tim McGilchrist <timmcgil@gmail.com>
2022-04-01 13:39:47 +1100

    Add wrapped date and date time types.

--- lib/gitlab_json.ml
+++ lib/gitlab_json.ml
@@ -6,4 +6,34 @@ module Adapter = struct
   module WebhookEvent = Atdgen_runtime.Json_adapter.Type_field.Make (struct
     let type_field_name = "object_kind"
   end)
+
+end
+
+module Date  = struct
+  type t = float
+
+  let wrap str =
+    try ISO8601.Permissive.date str
+    with Failure _ ->
+        raise 
+          (Ezjsonm.Parse_error
+             ( `String str,
+               Format.sprintf "%s: Date.wrap can't parse date format YYYY-MM-DD" str))
+
+  let unwrap = ISO8601.Permissive.string_of_date
+end
+
+module DateTime = struct
+  type t = float
+
+  let wrap str = 
+    try ISO8601.Permissive.datetime str
+      with Failure _ ->
+      raise
+        (Ezjsonm.Parse_error
+           ( `String str,
+             Format.sprintf "%s: DateTime.wrap can't parse ISO8601 date" str ))
+
+  let unwrap date =
+      ISO8601.Permissive.string_of_datetimezone(date, 0.)
 end
